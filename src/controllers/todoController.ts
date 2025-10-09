72166dc37ce21a5b1686f79aee3047c3a5c74410
Rupam-It <mannarupam3@gmail.com>
2025-10-09 12:01:05 +0530

    the controller is also added
    Signed-off-by: Rupam-It <mannarupam3@gmail.com>

---
+++ src/controllers/todoController.ts
@@ -0,0 +1,14 @@
+import { Request , Response} from "express";
+import  Todo  from "../models/todoModel.js";
+
+
+export const addTodo= async (req:Request, res: Response)=>{
+try{
+    const {title} =req.body
+    const newTodo = new Todo({ title });
+    await newTodo.save();
+    res.status(201).json({ message: "Todo added successfully", todo: newTodo });
+}catch(error){
+    res.status(500).json({ message: "Error adding todo", error });
+}
+}
