8b144021c07a526779ea09f16a9d39e4843de76f
Rupam-It <mannarupam3@gmail.com>
2025-10-11 13:39:42 +0530

    already added the update and delte commands
    Signed-off-by: Rupam-It <mannarupam3@gmail.com>

--- src/routes/todoRoutes.ts
+++ src/routes/todoRoutes.ts
@@ -1,10 +1,13 @@
 import express from "express"
 
-import { addTodo, getTodo } from "../controllers/todoController"
+import { addTodo, getTodo,updateTodo,deleteTodo } from "../controllers/todoController"
 
 const router= express.Router()
 
 router.post("/add",addTodo)
 router.get("/",getTodo)
+router.put("/update/:id",updateTodo)
+router.delete("/delete/:id", deleteTodo);
+
 
 export default router
